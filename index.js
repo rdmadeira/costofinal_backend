3b4f4831b9d03a6899bb5340c9be372d482ca0f7
rdmadeira <rdmadeira2@gmail.com>
2023-03-30 17:54:12 -0300

    add an argument and separate command

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-/* eslint-disable no-unsafe-optional-chaining */
+
 import { Command } from 'commander';
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import figlet from 'figlet';
+import { v4 as uuidv4 } from 'uuid';
 
-import { productTypePrompt } from './prompts.js';
+import { productPrompt, productTypePrompt } from './prompts.js';
 import { getProducts, saveProducts } from './utils.js';
 
 const program = new Command();
@@ -19,17 +20,47 @@ program
 program
   .command('newgroup')
   .alias('ng')
-  .description(chalk.bgGreen('Agregue nuevo grupo de productos o un producto'))
+  .description(chalk.green('Agregue nuevo grupo de productos'))
   .action(() => {
-    prompt(productTypePrompt)
+    console.log(`
+    Por favor insira el nombre del nuevo 
+    subgrupo de productos:
+
+    `);
+    prompt(productTypePrompt).then(({ productType }) => {
+      const products = getProducts() || {};
+      products[productType] = {};
+      saveProducts(products);
+    });
+  });
+
+program
+  .command('newproduct')
+  .alias('np')
+  .description(
+    chalk.green('Cria un nuevo producto, adentro del subgrupo pasado')
+  )
+  .arguments('<producttype>')
+  .action(() => {
+    console.log(`
+      Por favor insira en los campos, los parametros pedidos:
+       - Nombre Producto -> insira el nombre del producto
+       - Codigo del Producto -> insira el codigo del producto
+       - Medida de Producto -> insira si es el caso
+       - Precio -> insira un numero de precio de producto, sin simbolos
+    `);
+    const productType = process.argv[3];
+
+    prompt(productPrompt)
       .then((answers) => {
+        const products = getProducts() || {};
         const answersProduct = {
           codigo: answers.codigo,
           medida: answers.medida,
           precio: answers.precio,
+          id: uuidv4(),
         };
-        const productType = answers.productType;
-        const products = getProducts() || {};
+
         const productsSizeArray =
           products[productType]?.items[answers.productName] || [];
 
--- prompts.js
+++ prompts.js
@@ -1,9 +1,12 @@
 const productTypePrompt = [
   { name: 'productType', message: 'Grupo de producto' },
+];
+
+const productPrompt = [
   { name: 'productName', message: 'Nombre Producto' },
   { name: 'codigo', message: 'Codigo del Producto' },
   { name: 'medida', message: 'Medida del producto' },
   { type: 'number', name: 'precio', message: 'Precio del producto' },
 ];
 
-export { productTypePrompt };
+export { productTypePrompt, productPrompt };
